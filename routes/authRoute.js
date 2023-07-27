d567937336c9e8130401c5af41e0adbea5265152
MdSharib <mdsharib15041998@gmail.com>
2023-07-27 23:39:31 +0530

    added all auth routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,4 @@
 import express from "express";
-import {
-  registerController,
-  loginController,
-  forgotPasswordController,
-} from "../controllers/authController.js";
 
 //router object
 const router = express.Router();
@@ -17,6 +12,36 @@ router.post("/login", loginController);
 
 //Forgot Password || POST
 router.post("/forgot-password", forgotPasswordController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all user orders for admin
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update for admin
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
 
