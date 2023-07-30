e0616a19bf05725b05aa3cf28083119bbc40fe92
MdSharib <mdsharib15041998@gmail.com>
2023-07-30 22:45:10 +0530

    added auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,15 @@
 import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -14,13 +25,14 @@ router.post("/login", loginController);
 router.post("/forgot-password", forgotPasswordController);
 
 //test routes
+// requiresignin and isAdmin both are middleware which helps in authetication tokens, and if not matched, doesnot show the request route
 router.get("/test", requireSignIn, isAdmin, testController);
 
 //protected User route auth
+//for dashboard- if ok true then only show dashboad.
 router.get("/user-auth", requireSignIn, (req, res) => {
   res.status(200).send({ ok: true });
 });
-
 //protected Admin route auth
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
