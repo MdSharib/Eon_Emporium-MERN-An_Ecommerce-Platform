9d8da8546f26bdfd4c2e4c78b31914828627d65a
MdSharib <mdsharib15041998@gmail.com>
2023-08-07 21:51:37 +0530

    added order and user model

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+//defining schema to be sent to users under ecomm
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: {},
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } //new user timestamp record 
+);
+
+export default mongoose.model("users", userSchema); //adding schema into the users section under ecomm in mongoDb
