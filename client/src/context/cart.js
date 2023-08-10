d8ca76a771f647b19e0e21997ac297601547b0e7
MdSharib <mdsharib15041998@gmail.com>
2023-08-10 07:32:55 +0530

    added layouts routes context

---
+++ client/src/context/cart.js
@@ -0,0 +1,22 @@
+import { useState, useContext, createContext, useEffect } from "react";
+
+const CartContext = createContext();
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => { // to check and get cart items from local storage coz if we add items in cart and refersh, the cart items should be there in local storage
+    let existingCartItem = localStorage.getItem("cart");
+    if (existingCartItem) setCart(JSON.parse(existingCartItem));
+  }, []);
+
+  return (
+    <CartContext.Provider value={[cart, setCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+// custom hook
+const useCart = () => useContext(CartContext);
+
+export { useCart, CartProvider };
